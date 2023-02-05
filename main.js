0d7300b18c396667ecda9dcc001018c78cba0445
keziarkts <113933860+keziarkts@users.noreply.github.com>
2023-02-05 14:12:41 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function showSocial() {
+  var divShow = document.getElementById("social-list");
+  if(divShow.style.display === "none") {
+    divShow.style.display = "block";
+  } else {
+    divShow.style.display = "none";
+  }
+}
